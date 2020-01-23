575a8b3b75f5ac393cb047e3cfb05ef6034a4259
pilwoo6866 <60229502+pilwoo6866@users.noreply.github.com>
2020-01-24 00:56:08 +0900

    first commit

---
+++ Test/Assets/Scripts/Active.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Active : MonoBehaviour
+{
+    public GameObject gameObject;
+    
+    public void setActive(bool activated)
+    {
+        gameObject.SetActive(activated);
+    }
+}
